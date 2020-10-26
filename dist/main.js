dd5dc8ada0d151913295e360ae2349e67c3e2398
HayBeeCoder <Oguntadeabass1@gmail.com>
2020-10-26 06:37:40 +0000

    Initial Completion

--- dist/main.js
+++ dist/main.js
@@ -1,9 +1,14 @@
 const toggle = document.querySelector(".toggle");
+function select(item) {
+    return document.querySelector(item);
+}
 toggle.addEventListener("change" , toggleTheme)
 function toggleTheme(e) {
     if (toggle.checked) {
         document.body.className = 'dragon';
+        select('.mode').textContent = "Dark Mode"
     } else {
         document.body.className = 'unicorn';
+        select('.mode').textContent = "Light Mode"
     }
 }
