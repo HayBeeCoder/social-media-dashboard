81d7e30f85c5bb652d0848cd9a95fd3c31e7181b
HayBeeCoder <Oguntadeabass1@gmail.com>
2020-11-24 02:29:38 +0000

    feat: update README.md

--- dist/main.js
+++ dist/main.js
@@ -2,7 +2,7 @@ const toggle = document.querySelector(".toggle");
 function select(item) {
     return document.querySelector(item);
 }
-toggle.addEventListener("change" , toggleTheme)
+toggle.addEventListener("change", toggleTheme)
 function toggleTheme(e) {
     if (toggle.checked) {
         document.body.className = 'dragon';
